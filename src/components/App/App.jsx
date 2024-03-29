0dbe8ce300c8e1d3056aa03dfaf5a7215e030772
NazarHook <145909670+NazarHook@users.noreply.github.com>
2024-03-30 01:35:11 +0200

    uncommented

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,14 +12,14 @@ import transactions from '../../transactions.json'
 export default function App () {
   return (
     <>
-      {/* <Profile
+      <Profile
         name={userData.username}
         tag={userData.tag}
         location={userData.location}
         image={userData.avatar}
         stats={userData.stats}
-      /> */}
-      {/* <FriendList friends={friends} /> */}
+      />
+      <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </>
   );
